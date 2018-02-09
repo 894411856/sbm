a18bcf179fbc68b9a7f5a838a0a2e02f6972f556
lx <894411856@qq.com>
2018-02-09 16:33:00 +0800

    Initial commit

---
+++ src/test/java/com/openwan/service/CityServiceTest.java
@@ -0,0 +1,47 @@
+package com.openwan.service;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+/**
+ * Created by lujun.chen on 2017/3/13.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+public class CityServiceTest {
+
+	private static Logger logger = LoggerFactory
+			.getLogger(CityServiceTest.class);
+
+
+
+	@Test
+	public void selectquery() {
+
+	}
+	
+	
+	
+	 
+	@Test
+	public void sss () throws IOException{
+
+	}
+}
